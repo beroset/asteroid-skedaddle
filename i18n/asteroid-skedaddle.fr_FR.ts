841a41192d705d8b77795df242cbeee6b2652e41
Ed Beroset <beroset@ieee.org>
2026-01-09 20:08:08 -0500

    Add French language translation
    Co-authored-by: killenny45 <https://github.com/killenny45>
    Signed-off-by: Ed Beroset <beroset@ieee.org>

---
+++ i18n/asteroid-skedaddle.fr_FR.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name></name>
+    <message id="id-speech">
+        <location filename="../src/Settings.qml" line="26"/>
+        <source>Speech</source>
+        <extracomment>Speech settings title</extracomment>
+        <translation>Vocaux</translation>
+    </message>
+    <message id="id-use-miles">
+        <location filename="../src/Settings.qml" line="51"/>
+        <source>Use miles</source>
+        <extracomment>Use miles instead of kilometers as unit</extracomment>
+        <translation>Utiliser les miles</translation>
+    </message>
+    <message id="id-distance">
+        <location filename="../src/Announcer.qml" line="54"/>
+        <source>Distance:</source>
+        <extracomment>Spoken word for distance</extracomment>
+        <translation>Distance:</translation>
+    </message>
+    <message id="id-frac-miles">
+        <location filename="../src/Announcer.qml" line="59"/>
+        <source>%1 miles</source>
+        <extracomment>fractional distance in miles</extracomment>
+        <translation>%1 miles</translation>
+    </message>
+    <message id="id-frac-distance">
+        <location filename="../src/Announcer.qml" line="63"/>
+        <source>%1 kilometers</source>
+        <extracomment>fractional distance in km</extracomment>
+        <translation>%1 kilomètre</translation>
+    </message>
+    <message id="id-int-miles" numerus="yes">
+        <location filename="../src/Announcer.qml" line="69"/>
+        <source>%n mile(s)</source>
+        <extracomment>integer distance in km</extracomment>
+        <translation>
+            <numerusform>%n mile</numerusform>
+            <numerusform>%n miles</numerusform>
+        </translation>
+    </message>
+    <message id="id-int-distance" numerus="yes">
+        <location filename="../src/Announcer.qml" line="73"/>
+        <source>%n kilometer(s)</source>
+        <extracomment>integer distance in km</extracomment>
+        <translation>
+            <numerusform>%n kilomètre</numerusform>
+            <numerusform>%n kilomètres</numerusform>
+        </translation>
+    </message>
+    <message id="id-time">
+        <location filename="../src/Announcer.qml" line="79"/>
+        <source>Time:</source>
+        <extracomment>Spoken word for an elapsed time</extracomment>
+        <translation>Temps:</translation>
+    </message>
+    <message id="id-hours" numerus="yes">
+        <location filename="../src/Announcer.qml" line="84"/>
+        <location filename="../src/Announcer.qml" line="113"/>
+        <source>%n hour(s)</source>
+        <extracomment>Spoken elapsed hour(s)</extracomment>
+        <translation>
+            <numerusform>%n heure</numerusform>
+            <numerusform>%n heures</numerusform>
+        </translation>
+    </message>
+    <message id="id-minutes" numerus="yes">
+        <location filename="../src/Announcer.qml" line="89"/>
+        <location filename="../src/Announcer.qml" line="118"/>
+        <source>%n minute(s)</source>
+        <extracomment>Spoken elapsed minute(s)</extracomment>
+        <translation>
+            <numerusform>%n minute</numerusform>
+            <numerusform>%n minutes</numerusform>
+        </translation>
+    </message>
+    <message id="id-second" numerus="yes">
+        <location filename="../src/Announcer.qml" line="93"/>
+        <location filename="../src/Announcer.qml" line="122"/>
+        <source>%n second(s)</source>
+        <extracomment>Spoken elapsed seconds(s)</extracomment>
+        <translation>
+            <numerusform>%n seconde</numerusform>
+            <numerusform>%n secondes</numerusform>
+        </translation>
+    </message>
+    <message id="id-split-pace">
+        <location filename="../src/Announcer.qml" line="102"/>
+        <source>split pace</source>
+        <extracomment>Spoken split pace</extracomment>
+        <translation>Allure par fraction</translation>
+        <extra-For>half km or half mile</extra-For>
+    </message>
+    <message id="id-pace">
+        <location filename="../src/Announcer.qml" line="107"/>
+        <source>pace</source>
+        <extracomment>Spoken pace</extracomment>
+        <translation>Allure</translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-skedaddle.desktop.h" line="6"/>
+        <source>Running Logger App</source>
+        <oldsource>Hello World App</oldsource>
+        <translation>Application de suivi de course à pied</translation>
+    </message>
+    <message id="id-mile-abbrev">
+        <location filename="../src/RunDisplay.qml" line="49"/>
+        <source>mi</source>
+        <extracomment>Abbreviation for miles</extracomment>
+        <translation>mi</translation>
+    </message>
+    <message id="id-km-abbrev">
+        <location filename="../src/RunDisplay.qml" line="52"/>
+        <source>km</source>
+        <extracomment>Abbreviation for kilometers</extracomment>
+        <translation>km</translation>
+    </message>
+    <message id="id-sats">
+        <location filename="../src/RunDisplay.qml" line="93"/>
+        <source>sats</source>
+        <extracomment>label for number of satellites visible Note that this must be a short string</extracomment>
+        <translation>sats</translation>
+    </message>
+    <message id="id-sats-used">
+        <location filename="../src/RunDisplay.qml" line="100"/>
+        <source>used</source>
+        <extracomment>label for number of satellites used Note that this must be a short string</extracomment>
+        <translation>util.</translation>
+    </message>
+</context>
+</TS>
